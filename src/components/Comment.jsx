0e5750d0edffa88fef771bc2be610b922b1a91e0
hermeson <hermesondovalebeserra722@gmail.com>
2023-05-15 20:26:21 -0300

    adding the like Count

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import style from './Comment.module.css';
 import {ThumbsUp, Trash} from 'phosphor-react'
 
-export function Comment({content}){
+export function Comment({content, ondeleteComment}){
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeCounter() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    };
+
+    function handleDeleteComment() {
+        return (
+            ondeleteComment(content)
+        );
+    };
     return (
         <div className={style.comment}>
             <Avatar src="https://github.com/hermeson883.png" hasborder= {false}/>
@@ -14,16 +28,16 @@ export function Comment({content}){
                             <time title='11 de Maio às 08:13h' dateTime="2023-05-11 08:13:30">Cerca de 1H atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size = {20}/>
                         </button>
                     </header>
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeCounter}>
                         <ThumbsUp size={20} />
-                        Aplaudir  <span>20</span>
+                        Aplaudir  <span>{likeCount}</span>
                     </button>
                 </footer>
 
